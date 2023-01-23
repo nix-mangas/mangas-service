a2c06c70ba5bfb0c4e5a0c0d8c19cdca6a4f0838
renan5g <renansilva.dev5g@gmail.com>
2023-01-23 19:29:31 -0300

    fix: total count latest chapters

--- src/Infra/Database/Eloquent/Repositories/ChapterRepository.php
+++ src/Infra/Database/Eloquent/Repositories/ChapterRepository.php
@@ -58,7 +58,7 @@ class ChapterRepository implements IChapterRepository {
             'data'         => $mangas,
             "per_page"     => $perPage,
             "to"           => $toNextPage,
-            "total"        => $totalPages,
+            "total"        => $count,
             "last_page"    => $totalPages
         ];
     }
