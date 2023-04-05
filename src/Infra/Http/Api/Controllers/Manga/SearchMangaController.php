6c469970b12b406d24372b9877d72636d0df2b49
lyem <rfael403@gmail.com>
2023-04-04 22:08:23 -0300

    update cache

--- src/Infra/Http/Api/Controllers/Manga/SearchMangaController.php
+++ src/Infra/Http/Api/Controllers/Manga/SearchMangaController.php
@@ -24,10 +24,16 @@ class SearchMangaController extends Controller {
             'per_page' => $request?->query('per_page'),
         ];
 
-        if($request->query('search') == null) {
-            Cache::add('mangas-' . $request->query('order') ?? 'default' . $request?->query('per_page') ?? '10', $this->useCase->execute($filters), 10);
+        $key = 'mangas-' . $request->query('order') ?? 'default' . $request?->query('per_page') ?? '10';
 
-            return Cache::get('mangas-' . $request->query('order') ?? 'default' . $request?->query('per_page') ?? '10');
+        if($request->query('search') == null) {
+            if (Cache::has($key)) {
+                return Cache::get($key);
+            }
+    
+            Cache::put($key, $this->useCase->execute($filters), 60);
+            
+            return Cache::get($key);
         }
 
         return $this->useCase->execute($filters);
